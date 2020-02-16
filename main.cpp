085b378460038a4f238b2d1b18a3b07c9139285f
SkyVault <dustinneumann42@gmail.com>
2020-02-16 12:37:21 -0800

    Added a top menu bar

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <functional>
 #include <algorithm>
+#include <tuple>
 
 #include "stl_reader.h"
 
@@ -14,6 +15,8 @@
 
 #include "raygui.h"
 #include "rlgl.h"
+
+constexpr auto MENU_MARGIN {10};
 
 struct State {
     bool running = true;
@@ -26,6 +29,17 @@ struct State {
     Camera camera = {};
 
     std::vector<Model> models;
+
+    std::vector<std::tuple<std::string, std::vector<std::string>>>
+    top_menu_buttons {
+        {"File",
+         {"Load scene",
+          "Save scene",
+          "Import model",
+          "Exit"}},
+        {"Edit", {}},
+        {"Render", {}}
+    };
 };
 
 Model load_model(const std::string& path) {
@@ -49,21 +63,48 @@ void draw_model(const State& state, const Model& model) {
     DrawModel(model, pos, 0.1, RAYWHITE);
 }
 
-void draw_gui(const State& state) {
-    GuiPanel(Rectangle{0, 0, 200, 400});
+void do_menu_bar(State& state) {
+    GuiPanel(Rectangle{0, 0, GetScreenWidth(), 32});
 
-    if (GuiButton((Rectangle){0, 0, 100, 20}, "Hello World")) {
+    float cursor_x = MENU_MARGIN;
+
+    for (const auto& [txt, options] : state.top_menu_buttons) {
+
+        const auto w = MeasureText(txt.c_str(), GetFontDefault().baseSize);
+        const auto reg = Rectangle{cursor_x, 0, w, 32};
+        const auto mpos = GetMousePosition();
+
+        cursor_x += w + MENU_MARGIN;
+
+        bool show_drop_down =
+            CheckCollisionPointRec(mpos, reg);
+
+        if (GuiLabelButton(reg, txt.c_str())) {
+            show_drop_down = true;
+        }
+
 
     }
 }
 
+void draw_gui(State& state) {
+    do_menu_bar(state);
+
+    GuiPanel(Rectangle{0, 32, 200, 400});
+
+    GuiStatusBar(
+        (Rectangle){ 0, GetScreenHeight() - 20, GetScreenWidth(), 20 },
+        "");
+}
+
 int main () {
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE);
     InitWindow(1280, 720, "Hello World");
     SetTargetFPS(60);
 
     State state;
 
+    // Camera initialization
     state.camera.position = (Vector3){ 0.0f, 10.0f, 10.0f };
     state.camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
     state.camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
@@ -71,10 +112,12 @@ int main () {
     state.camera.type = CAMERA_PERSPECTIVE;
     SetCameraMode(state.camera, CAMERA_FREE); // Set a free camera mode
 
+    // Shader initialization
     state.shader = LoadShader("base_vs.glsl", "base_fs.glsl");
     state.model_loc = GetShaderLocation(state.shader, "model");
     state.light_pos_loc = GetShaderLocation(state.shader, "light_pos");
 
+    // TEMP
     state.models.push_back(load_model("models/z-assm.obj"));
 
     while (!WindowShouldClose() && state.running) {
